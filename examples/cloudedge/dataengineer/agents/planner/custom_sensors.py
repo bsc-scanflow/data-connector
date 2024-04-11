4c2ed1cb8d41f318c3fd94c46b23ffdd0cd7d66e
pliu <peini.liu@bsc.es>
2024-04-11 10:33:19 +0200

    remove tutor

--- examples/cloudedge/dataengineer/agents/planner/custom_sensors.py
+++ examples/cloudedge/dataengineer/agents/planner/custom_sensors.py
@@ -18,6 +18,25 @@ import time
 from scanflow.agent.sensors.sensor import sensor
 from scanflow.agent.sensors.sensor_dependency import sensor_dependency
 
+def tock():
+    print('Tock! The time is: %s' %  time.strftime("'%Y-%m-%d %H:%M:%S'"))
+
+#example 1: count number of predictions in last 5 min
+@sensor(nodes=["predictor"], filter_string="tags.mlflow.runName='predictor-batch' and metrics.n_predictions > 0")
+async def watch_qos(runs: List[mlflow.entities.Run], args, kwargs):
+    print(args)
+    print(kwargs)
+
+    n_predictions = list(map(lambda run: run.data.metrics['n_predictions'], runs))
+    
+    number_of_predictions = reduce(lambda x,y : x+y, n_predictions)
+    logging.info(f"count_number_of_predictions - {number_of_predictions}")
+
+    if number_of_predictions_threshold(number_of_predictions):
+       await call_analyze_check_predictions(run_ids = list(map(lambda run: run.info.run_id, runs)))
+
+    return number_of_predictions
+
 
 custom_sensor_router = APIRouter()
 
--- examples/cloudedge/dataengineer/workflows/postprocessing-batch/upload.py
+++ examples/cloudedge/dataengineer/workflows/postprocessing-batch/upload.py
@@ -3,6 +3,8 @@ import click
 import logging
 import pandas as pd
 import os
+import random
+import statistics
 
 import sys
 sys.path.insert(0, '/scanflow/scanflow')
@@ -20,7 +22,9 @@ def upload(name):
     mlflow.set_tracking_uri(client.get_tracker_uri(True))
     logging.info("Connecting tracking server uri: {}".format(mlflow.get_tracking_uri()))
 
-    qos_list = [25, 22, 20, 19, 15, 14]
+    qos_list_base = [25, 22, 20, 19, 15, 14]
+    # Adjust each QoS value by adding a random number between 0 and 2
+    qos_list = [value + random.uniform(0, 5) for value in qos_list_base]
     logging.info("dumy qosvalue: {}".format(qos_list))
 
     mlflow.set_experiment("predictor")
@@ -28,6 +32,19 @@ def upload(name):
         for i, number in enumerate(qos_list):
             # Log each number as a separate metric with a unique key (appending the index)
             mlflow.log_metric(key=f"qos_{i}", value=number)
-    
+
+        # Find the maximum QoS value and its index
+        max_qos = max(qos_list)
+        max_qos_index = qos_list.index(max_qos)
+        std_dev = statistics.stdev(qos_list)
+        population_std_dev = statistics.pstdev(qos_list)
+        
+        # Log the maximum QoS value
+        mlflow.log_metric(key="max_qos", value=max_qos)
+        mlflow.log_metric(key="std_dev", value=std_dev)
+        mlflow.log_metric(key="population_std_dev", value=population_std_dev)
+        # Log the index of the maximum QoS value as a parameter (or metric)
+        mlflow.log_param(key="max_qos_index", value=max_qos_index)
+        
 if __name__ == '__main__':
     upload()
