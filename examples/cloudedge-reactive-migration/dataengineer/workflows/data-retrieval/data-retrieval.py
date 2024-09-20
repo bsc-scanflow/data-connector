3350050696e07af2bcb6fe59974a659fdec6fce6
ProtossGP32 <19353116+ProtossGP32@users.noreply.github.com>
2024-09-20 13:36:59 +0200

    wip: data-retrieval script
    - Add `retrieve_prometheus_data` function --> Tested
    - Add `store_query_results` function --> Not tested

--- examples/cloudedge-reactive-migration/dataengineer/workflows/data-retrieval/data-retrieval.py
+++ examples/cloudedge-reactive-migration/dataengineer/workflows/data-retrieval/data-retrieval.py
@@ -5,17 +5,52 @@ import logging
 import pandas as pd
 import os
 
+from promcsv import PromCSV
+import json
+
 import sys
 sys.path.insert(0, '/scanflow/scanflow')
 from scanflow.client import ScanflowTrackerClient
 
-@click.command(help="Retrieve real-time data from Prometheus")
-# from main scanflow
-@click.option("--app_name", default=None, type=str)
-@click.option("--team_name", default=None, type=str)
-# TODO: Add PromCSV parameters
-def data_retrieval(app_name, team_name):
+def retrieve_prometheus_data(promcsv_config:click.File):
+    """
+    Retrieve Prometheus query results
+    """
+    logging.info("Retrieving Prometheus data...")
+    with promcsv_config as f:
+        query_config = json.load(f)
 
+    prom_query = PromCSV(**query_config)
+
+    # Launch the PromCSV queries
+    logging.info("Launching Prometheus queries...")
+    prom_query.retrieve_results()
+
+    # Save the PromCSV results
+    logging.info("Saving results to CSV file...")
+    prom_query.pivot_results()
+    prom_query.concat_results()
+    csv_filename = prom_query.to_csv()
+    
+    logging.info(f"CSV results stored in: {csv_filename}")
+    return csv_filename
+
+
+def store_query_results(app_name:str, team_name:str , query_results:str):
+    """
+    Store the query results as an MLflow artifact
+    ScanflowTrackerClient expects several environment variables to be already set:
+    - AWS_ACCESS_KEY_ID: Username to access the MinIO object store
+    - AWS_SECRET_ACCESS_KEY: Password to access the MinIO object store
+    - MLFLOW_S3_ENDPOINT_URL: URL to the MLflow MinIO object store API
+    - AWS_ENDPOINT_URL: URL to the MinIO object store API (same as MLFLOW_S3_ENDPOINT_URL)
+    - SCANFLOW_TRACKER_URI: URI to the Scanflow Tracker (MLflow)
+    - SCANFLOW_SERVER_URI: Same as SCANFLOW_TRACKER_URI
+    - SCANFLOW_TRACKER_LOCAL_URI: Same as SCANFLOW_TRACKER_URI
+    """
+    logging.info("Storing query results...")
+    # DEBUG:
+    logging.info(f"App name: {app_name}, Team name: {team_name}")
     # Create a ScanflowTrackerClient to manage any interaction with the Scanflow Tracker (?)
     client = ScanflowTrackerClient(verbose=True)
     # Why is this command needed?
@@ -24,8 +59,27 @@ def data_retrieval(app_name, team_name):
     mlflowclient = MlflowClient(client.get_tracker_uri(True))
     logging.info("Connecting tracking server uri: {}".format(mlflow.get_tracking_uri()))
 
+    # Set the MLflow experiment where to upload artifacts
+    mlflow.set_experiment("reactive-predictor")
+    # Use this method to upload the CSV as an MLFlow experiment artifact
     #client.save_app_artifacts()
+
+    # If we wanted to download artifacts from the MLFlow experiment we'd use this method
     #client.download_artifacts()
+
+@click.command(help="Retrieve real-time data from Prometheus")
+# from main scanflow
+@click.option("--app_name", default=None, type=str)
+@click.option("--team_name", default=None, type=str)
+# PromCSV arguments
+@click.option("--promcsv_config", default=None, type=click.File('rb'))
+def main(app_name, team_name, promcsv_config):
     
+    # Retrieve application metrics from Prometheus
+    csv_results = retrieve_prometheus_data(promcsv_config)
+
+    # Store CSV results as an experiment's artifact
+    store_query_results(app_name, team_name, csv_results)
+
 if __name__ == '__main__':
-    data_retrieval()
+    main()
