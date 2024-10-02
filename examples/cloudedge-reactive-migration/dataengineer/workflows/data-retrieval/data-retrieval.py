d7fdd7fb8fd0c099b441417c9d986c9462bf7e9f
Marc Palacín Marfil <marc.palacin@bsc.es>
2024-10-02 10:25:23 +0200

    upd: add custom output_path parameter to data-retrieval

--- examples/cloudedge-reactive-migration/dataengineer/workflows/data-retrieval/data-retrieval.py
+++ examples/cloudedge-reactive-migration/dataengineer/workflows/data-retrieval/data-retrieval.py
@@ -13,17 +13,21 @@ import sys
 sys.path.insert(0, '/scanflow/scanflow')
 from scanflow.client import ScanflowTrackerClient
 
-def retrieve_prometheus_data(promcsv_config:click.File):
+def retrieve_prometheus_data(promcsv_config:click.File, output_path:str = None) -> str:
     """
     Retrieve Prometheus query results
+    return: path to the generated CSV file
     """
     logging.info("Retrieving Prometheus data...")
     with promcsv_config as f:
         query_config = json.load(f)
 
     # Modify the PromCSV output directory so each execution creates a different subdir
-    # - This is done to prevent uploading historical results as artifacts in each experiment iteration
-    query_config["output_dir"] = os.path.join(query_config['output_dir'], f"run_at_{round(datetime.now().timestamp())}")
+    # - If output_path is provided, we ignore the value included in the promcsv_config file
+    if output_path:
+        query_config["output_dir"] = os.path.join(output_path, f"run_at_{round(datetime.now().timestamp())}")
+    else:
+        query_config["output_dir"] = os.path.join(query_config["output_dir"], f"run_at_{round(datetime.now().timestamp())}")
 
     prom_query = PromCSV(**query_config)
 
@@ -41,7 +45,7 @@ def retrieve_prometheus_data(promcsv_config:click.File):
     return csv_filename
 
 
-def store_query_results(app_name:str, team_name:str , query_results:str):
+def store_query_results(app_name:str, team_name:str , query_results:str) -> None:
     """
     Store the query results as an MLflow artifact
     ScanflowTrackerClient expects several environment variables to be already set:
@@ -52,8 +56,9 @@ def store_query_results(app_name:str, team_name:str , query_results:str):
     - SCANFLOW_TRACKER_URI: URI to the Scanflow Tracker (MLflow)
     - SCANFLOW_SERVER_URI: Same as SCANFLOW_TRACKER_URI
     - SCANFLOW_TRACKER_LOCAL_URI: Same as SCANFLOW_TRACKER_URI
+    return: None
     """
-    # Create a ScanflowTrackerClient to manage any interaction with the Scanflow Tracker (?)
+    # Create a ScanflowTrackerClient to manage any interaction with the Scanflow Tracker
     client = ScanflowTrackerClient(verbose=True)
 
     # Upload the experiment's results as artifacts
@@ -69,14 +74,22 @@ def store_query_results(app_name:str, team_name:str , query_results:str):
 @click.option("--app_name", default=None, type=str)
 @click.option("--team_name", default=None, type=str)
 # PromCSV arguments
+@click.opton("--output_path", default="/workflow", type=str)
 @click.option("--promcsv_config", default="/app/data-retrieval/promql_queries.json", type=click.File('rb'))
-def main(app_name, team_name, promcsv_config):
+def main(app_name, team_name, output_path, promcsv_config):
     
     # Retrieve application metrics from Prometheus
-    csv_results = retrieve_prometheus_data(promcsv_config)
+    csv_results = retrieve_prometheus_data(
+        promcsv_config=promcsv_config,
+        output_path=output_path
+    )
 
     # Store CSV results as an experiment's artifact
-    store_query_results(app_name, team_name, csv_results)
+    store_query_results(
+        app_name=app_name,
+        team_name=team_name,
+        query_results=csv_results
+    )
 
 if __name__ == '__main__':
     main()
