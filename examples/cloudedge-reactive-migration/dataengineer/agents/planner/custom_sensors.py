f4ccadecf522b83f8354bf0d59fea75ef2e2267d
Marc Palacín Marfil <marc.palacin@bsc.es>
2024-11-14 11:33:16 +0100

    chore: fix PEP8 warnings

--- examples/cloudedge-reactive-migration/dataengineer/agents/planner/custom_sensors.py
+++ examples/cloudedge-reactive-migration/dataengineer/agents/planner/custom_sensors.py
@@ -1,42 +1,43 @@
 import mlflow.entities
-from .custom_rules import *
-from .custom_actuators import *
-from typing import List, Optional
+from custom_rules import *
+from custom_actuators import *
+from typing import List
 import logging
+import time
+from scanflow.agent.sensors.sensor import sensor
+import mlflow
+import json
+
 logging.basicConfig(format='%(asctime)s -  %(levelname)s - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S')
 logging.getLogger().setLevel(logging.INFO)
 
-import time
-
-from scanflow.agent.sensors.sensor import sensor
-import mlflow
 
 def tock():
-    print('Tock! The time is: %s' %  time.strftime("'%Y-%m-%d %H:%M:%S'"))
+    print('Tock! The time is: %s' % time.strftime("'%Y-%m-%d %H:%M:%S'"))
 
-#example 1: count number of predictions in last 5 min
+
 # TODO: parameterize the node names (use app_name for this and team_name for the run names)
 @sensor(nodes=["cloudedge-migration-experiment-ci"])
 async def reactive_watch_qos(runs: List[mlflow.entities.Run], args, kwargs):
     print(args)
     print(kwargs)
-    
-    migration_result = "No migration"
+
+    # migration_result = "No migration"
 
     # Only take into account the latest run and only if parameter "analysed" is set to false
     if runs:
         latest_run = runs[0]
-        if not "analysed" in latest_run.data.params:
-            
-            # Initialize an MLflow RunStatus object just for convenience
+        if "analysed" not in latest_run.data.params:
+
+            # Initialize an MlFlow RunStatus object just for convenience
             mlflow_run_status = mlflow.entities.RunStatus()
             # Add an availability/termination run timeout
             timeout = kwargs["timeout"] if "timeout" in kwargs else 60
-            
+
             start_time = time.time()
             elapsed_time = time.time() - start_time
-            
+
             # Wait for experiment parameters and metrics to be available
             logging.info(f"Waiting for run metrics and params to be available...")
             while "max_cluster" not in latest_run.data.params and elapsed_time < timeout:
@@ -49,18 +50,19 @@ async def reactive_watch_qos(runs: List[mlflow.entities.Run], args, kwargs):
             if elapsed_time > timeout:
                 logging.info("Current run still hasn't finished. Can't retrieve QoS values, exiting...")
                 return "No QoS values available"
-            
+
             # Retrieve the available experiment metrics and parameters
             max_cluster = latest_run.data.params["max_cluster"]
             max_qos = latest_run.data.metrics["max_qos"]
             # Index not required as of now
-            max_idx = latest_run.data.metrics["max_idx"]
+            # max_idx = latest_run.data.metrics["max_idx"]
 
             # Check if there's a max_cluster ID value, or it is set to None
             if (max_cluster != "None" and max_qos >= 0) and qos_constraints(max_qos):
 
                 # Proceed to migrate the application
-                logging.info(f"Maximum QoS value {max_qos} found in Cluster ID {max_cluster} is above SLA. Executing application migration...")
+                logging.info(
+                    f"Maximum QoS value {max_qos} found in Cluster ID {max_cluster} is above SLA. Executing application migration...")
 
                 migration_result = migrate_application(
                     app_name=kwargs["app_name"],
@@ -76,18 +78,19 @@ async def reactive_watch_qos(runs: List[mlflow.entities.Run], args, kwargs):
                     indent=2
                 )
             elif max_qos < 0:
-                logging.info("No QoS values available! Either the application is not deployed or there are no dlpipelines running")
                 migration_result = "No QoS values available! Either the application is not deployed or there are no dlpipelines running"
+                logging.info(migration_result)
+
             else:
                 # Nothing to do
-                logging.info("QoS below SLA. No migration required.")
                 migration_result = "QoS below SLA. No migration required"
-            
+                logging.info(migration_result)
+
             # Mark the experiment as already analysed
             # TODO: check if set_experiment is enough to avoid active run vs environment run issues
             logging.info(f"Experiment id: {latest_run.info.experiment_id}")
             logging.info(f"Experiment run id: {latest_run.info.run_id}")
-            
+
             # Set latest experiment as the active one
             mlflow.set_experiment(experiment_id=latest_run.info.experiment_id)
 
@@ -112,9 +115,9 @@ async def reactive_watch_qos(runs: List[mlflow.entities.Run], args, kwargs):
 
             logging.info(f"Setting experiment run {latest_run.info.run_id} as analysed...")
             with mlflow.start_run(
-                run_id=latest_run.info.run_id,
-                experiment_id=latest_run.info.experiment_id
-                ):
+                    run_id=latest_run.info.run_id,
+                    experiment_id=latest_run.info.experiment_id
+            ):
                 mlflow.log_param(
                     key="analysed",
                     value="True"
