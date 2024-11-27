3787763dca167cd7b2197115f4e084738b3fede5
Joan Oliveras Torra <joan.oliveras@bsc.es>
2024-11-27 18:33:10 +0100

    fix: fix format_parameters scanflow/tools/param.py Accepts empty parameters

--- scanflow/tools/param.py
+++ scanflow/tools/param.py
@@ -1,8 +1,9 @@
 def format_parameters(params: dict):
     list_params = list()
-    for k, v in params.items():
-        list_params.append(f"--{k}")
-        list_params.append(f"{v}")
+    if params is not None:
+        for k, v in params.items():
+            list_params.append(f"--{k}")
+            list_params.append(f"{v}")
 
     return list_params
 
