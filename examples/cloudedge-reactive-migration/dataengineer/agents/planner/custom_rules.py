d9b0bf4098f42adb299022a314e46b04774f2949
Marc Palacín Marfil <19353116+ProtossGP32@users.noreply.github.com>
2024-12-16 11:33:24 +0100

    upd: decrease Cloud SLA from 100ms to 50ms

--- examples/cloudedge-reactive-migration/dataengineer/agents/planner/custom_rules.py
+++ examples/cloudedge-reactive-migration/dataengineer/agents/planner/custom_rules.py
@@ -6,7 +6,7 @@ def qos_check(qos: float, cluster_type: str) -> bool:
     """
     Return true if the application's QoS should run elsewhere, false otherwise
     - If cluster_type is 'edge', then return True if QoS is above 200ms to move to the Cloud
-    - If cluster_type is 'cloud', then return True if QoS is below 100ms to bring the app back to the Edge
+    - If cluster_type is 'cloud', then return True if QoS is below 50ms to bring the app back to the Edge
     :param qos - QoS value of the application
     :param cluster_type - Type of cluster (edge, cloud)
     :return Boolean value
@@ -15,7 +15,7 @@ def qos_check(qos: float, cluster_type: str) -> bool:
         case "edge":
             return qos > 0.2
         case "cloud":
-            return qos < 0.1
+            return qos < 0.05
         case _:
             # In case the cluster type is unknown, play it safe and don't trigger the migration
             return False
