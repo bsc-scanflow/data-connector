ac48aaf72c444a4123c30453c28f0f423c71c60c
Marc Palacín Marfil <19353116+ProtossGP32@users.noreply.github.com>
2025-05-21 16:00:01 +0200

    wip: qos-upload for Proactive DataEngineer migration
    - Updated qos-upload script.
    - Added Dockerfile.
    - TODO: include qos-upload stage to DataEngineer pipeline.

--- examples/cloudedge-proactive-migration/dataengineer/workflows/qos-upload/qos-upload.py
+++ examples/cloudedge-proactive-migration/dataengineer/workflows/qos-upload/qos-upload.py
@@ -7,6 +7,9 @@ import shutil
 import fnmatch
 import sys
 import json
+# Required imports for HTTP requests
+# - requests already available in scanflow-executor docker image
+import requests
 
 sys.path.insert(0, '/scanflow/scanflow')
 from scanflow.client import ScanflowTrackerClient
@@ -34,21 +37,27 @@ def get_latest_file(files_path:str, file_ext:str = "*") -> str:
     logging.info(f"Latest CSV file found: {latest_file}")
     return latest_file
 
-def get_latest_experiment_run_id(experiment_name: str = None, run_name: str = None) -> str:
+def get_latest_experiment_run_id(experiment_name: str = None, run_name: str = None, run_age: int = 300) -> str:
     """
     Return the latest experiment run id
     return: run_id hash
     """
+    from datetime import datetime, timedelta
+
     # Get the experiment id
     reactive_experiment = mlflow.get_experiment_by_name(experiment_name)
     experiment_id = reactive_experiment.experiment_id
 
     # Retrieve filtered experiment runs by run_name, ordered by descending end time --> First entry will be the most recent
+    # WIP: reduce number of runs search to the last 5 minutes
+    starttimeframe = int((datetime.now() - timedelta(seconds=run_age)).timestamp() * 1000)
+    filter_string = f"run_name='{run_name}' and attributes.created > {str(starttimeframe)}"
     runs_df = mlflow.search_runs(
         experiment_ids=[experiment_id],
-        filter_string=f"run_name='{run_name}'",
+        filter_string=filter_string,
         order_by=["end_time DESC"]
     )
+    logging.info(f"Number of experiments fetched: {len(runs_df.index)}")
     # First row is the most recently finalized one
     run_id = runs_df.loc[[0]]['run_id'][0]
     return run_id
@@ -108,6 +117,44 @@ def purge_local_experiment_results(filename: str = None) -> None:
         shutil.rmtree(os.path.dirname(filename))
 
 
+
+def send_reactive_qos_analysis_request(
+    analysis_agent_uri: str,
+    nearbyone_service_name: str,
+    nearbyone_env_email: str,
+    nearbyone_env_password: str,
+    nearbyone_env_name: str,
+    nearbyone_organization_id: str) -> None:
+    """
+    Send a POST request to the Planner Agent sensor to analyse the Reactive migration QoS
+    """
+    # Compose the POST data and headers
+    data = {
+        "args": [],
+        "kwargs": {
+            "app_name": nearbyone_service_name,
+            "nearbyone_env_email": nearbyone_env_email,
+            "nearbyone_env_password": nearbyone_env_password,
+            "nearbyone_env_name": nearbyone_env_name,
+            "nearbyone_organization_id": nearbyone_organization_id
+        }
+    }
+    headers = {
+        "Content-type": "application/json"
+    }
+
+    # Send the POST request
+    try:
+        r = requests.post(
+            url=analysis_agent_uri,
+            json=data,
+            headers=headers
+            )
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+
+
 @click.command(help="Postprocessing: upload qos")
 @click.option("--name", default=None, type=str)
 @click.option("--app_name", default=None, type=str)
@@ -115,7 +162,25 @@ def purge_local_experiment_results(filename: str = None) -> None:
 @click.option("--csv_path", default="/workflow", type=str)
 @click.option("--csv_sep", default=";", type=str)
 @click.option("--purge_local_results", default=False, type=bool)
-def upload(name: str, app_name: str, team_name: str, csv_path: str, csv_sep: str, purge_local_results:bool) -> None:
+@click.option("--analysis_agent_uri", default=None, type=str)
+@click.option("--nearbyone_service_name", default=None, type=str)
+@click.option("--nearbyone_env_email", default=None, type=str)
+@click.option("--nearbyone_env_password", default=None, type=str)
+@click.option("--nearbyone_env_name", default=None, type=str)
+@click.option("--nearbyone_organization_id", default=None, type=str)
+def upload(
+    name: str,
+    app_name: str,
+    team_name: str,
+    csv_path: str,
+    csv_sep: str,
+    purge_local_results:bool,
+    analysis_agent_uri: str,
+    nearbyone_service_name: str,
+    nearbyone_env_email: str,
+    nearbyone_env_password: str,
+    nearbyone_env_name: str,
+    nearbyone_organization_id: str) -> None:
     """
     Upload the average QoS from the available latency values in the latest experiment results.
     ScanflowTrackerClient expects several environment variables to be already set:
@@ -129,6 +194,7 @@ def upload(name: str, app_name: str, team_name: str, csv_path: str, csv_sep: str
     """
     logging.info("Workflow step: {}".format(name))
 
+    logging.info("Retrieving latest metrics CSV file...")
     # Retrieve latest experiment CSV file
     csv_filename = get_latest_file(csv_path, "csv")
 
@@ -139,6 +205,7 @@ def upload(name: str, app_name: str, team_name: str, csv_path: str, csv_sep: str
     )
 
     # Initialize the ScanflowTrackerClient for MLflow to retrieve the Tracker URI
+    logging.info("Retrieving latest experiment run id...")
     client = ScanflowTrackerClient()
     mlflow.set_tracking_uri(client.get_tracker_uri(True))
     
@@ -152,6 +219,7 @@ def upload(name: str, app_name: str, team_name: str, csv_path: str, csv_sep: str
     )
 
     # Attach ALL QoS with indexed cluster_id to the latest experiment run. The max one might be from the previous app's cluster after a migration
+    logging.info("Uploading QoS values...")
     with mlflow.start_run(run_id=run_id):
         max_qos = 0
         max_cluster = "None"
@@ -170,13 +238,24 @@ def upload(name: str, app_name: str, team_name: str, csv_path: str, csv_sep: str
         mlflow.log_param(key="max_cluster", value=max_cluster)
         mlflow.log_metric(key="max_idx", value=max_idx)
 
+    # WIP: Send the QoS analysis request
+    logging.info("Sending QoS analysis request...")
+    send_reactive_qos_analysis_request(
+        analysis_agent_uri=analysis_agent_uri,
+        nearbyone_service_name=nearbyone_service_name,
+        nearbyone_env_email=nearbyone_env_email,
+        nearbyone_env_password=nearbyone_env_password,
+        nearbyone_env_name=nearbyone_env_name,
+        nearbyone_organization_id=nearbyone_organization_id
+    )
+
     # Purge local CSV files if requested
     # - This is useful to avoid large lists of files when looking for the latest one
     if purge_local_results:
         purge_local_experiment_results(
             filename=csv_filename
         )
-
+    
 
 if __name__ == '__main__':
     upload()
